2e5ccaf1135013e43d1d909f4f768679e498c2ff
Trollninja228 <timur5667262005@gmail.com>
2026-09-05 13:31:28 +0500

    db testing

--- back-end/WaterSampling/WaterSampling/settings.py
+++ back-end/WaterSampling/WaterSampling/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
 import sys
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,11 +107,14 @@ WSGI_APPLICATION = 'WaterSampling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
